9dd26d6207c3614a9f11f12d6fb7f16b7f73b56d
George Davidson <gd240673@gmail.com>
2023-02-26 17:56:16 -0500

    Update

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Window Looper
 
-Loop through multiple windows and send inputs.
+Loop through multiple windows and send a random phrase.
+
+## Instructions
+
+### Install Dependencies
+
+Install the necessary dependencies.
+
+```python
+pip install -r requirements.txt
+```
+
+### How To Use
+
+Press "F6" to start and stop the script.
+
+### Things To Note
+
+The script will never repeat the same phrase twice.
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import random
 import threading
 import time
 
-from win32gui import GetWindowText, IsWindowVisible, EnumWindows, SetForegroundWindow
-from pynput import keyboard
+import pynput
+import win32gui
 
 window_name = "Command Prompt"
 start_stop_key = "Key.f6"
@@ -13,41 +14,74 @@ class Main:
     def __init__(self):
         self.running = True
 
+        self.phrases = []
+
+        self.last_phrase = ""
+
+        with open("phrases.txt") as phrases_file:
+            for line in phrases_file:
+                self.phrases.append(line.strip())
+
     def get_window_names(self):
         window_names = []
 
         def winEnumHandler(hwnd, ctx):
-            if (IsWindowVisible(hwnd)):
-                window_names.append(GetWindowText(hwnd))
+            if (win32gui.IsWindowVisible(hwnd)):
+                window_names.append(win32gui.GetWindowText(hwnd))
 
-        EnumWindows(winEnumHandler, None)
+        win32gui.EnumWindows(winEnumHandler, None)
 
         return [string for string in window_names if string != ""]
 
-    def get_window_handles(self, window_name):
+    def get_window_handles(self):
         window_handles = []
 
         def winEnumHandler(window_handle, ctx):
-            if (IsWindowVisible(window_handle) and GetWindowText(window_handle) == window_name):
+            if (win32gui.IsWindowVisible(window_handle) and win32gui.GetWindowText(window_handle) == window_name):
                 window_handles.append(window_handle)
 
-        EnumWindows(winEnumHandler, None)
+        win32gui.EnumWindows(winEnumHandler, None)
 
         return window_handles
+
+    def random_phrase(self):
+        if len(self.phrases) != 0:
+            self.phrase_index = random.randint(0, len(self.phrases) - 1)
+            phrase = self.phrases[self.phrase_index]
+
+            if self.last_phrase.lower() == phrase.lower():
+                if len(self.phrases) > 1:
+                    if self.phrase_index == 0:
+
+                        phrase = self.phrases[self.phrase_index + 1]
+                    elif self.phrase_index == len(self.phrases) - 1:
+                        phrase = self.phrases[self.phrase_index - 1]
+                    else:
+                        phrase = self.phrases[self.phrase_index + 1]
+
+            random_number = random.randint(0, 2)
+
+            if random_number == 0:
+                phrase = phrase.lower()
+            elif random_number == 1:
+                phrase = phrase.upper()
+            else:
+                phrase = phrase.title()
+
+            self.last_phrase = phrase
+
+            return phrase
 
     def main_loop(self):
         while True:
             if self.running:
-                for handle in self.get_window_handles(window_name):
+                for handle in self.get_window_handles():
                     if not self.running:
                         break
 
-                    SetForegroundWindow(handle)
+                    win32gui.SetForegroundWindow(handle)
 
-                    keyboard.Controller().type("python")
-                    keyboard.Controller().tap(keyboard.Key.enter)
-                    keyboard.Controller().type("quit()")
-                    keyboard.Controller().tap(keyboard.Key.enter)
+                    pynput.keyboard.Controller().type(self.random_phrase())
 
                     time.sleep(window_switch_delay)
 
@@ -57,7 +91,7 @@ class Main:
 
 
 def keyboard_listener(main):
-    with keyboard.Listener(on_press=lambda key: main.on_press(key)) as keyboard_listener:
+    with pynput.keyboard.Listener(on_press=lambda key: main.on_press(key)) as keyboard_listener:
         keyboard_listener.join()
 
 
@@ -73,22 +107,11 @@ def main():
     main_loop_thred.daemon = True
     main_loop_thred.start()
 
-    while True:
-        if len(main.get_window_handles()) == 0:
-            print("Error: No Handles Found")
-            break
-
-        command = input("Type 'quit' to quit: -> ").lower()
-
-        if command == "quit":
-            print("Quitting...")
-            break
-
-        else:
-            print(f"'{command}' is not recognized. Type 'quit' to quit.")
+    if len(main.get_window_handles()) == 0:
+        print("error: no handles found")
+    else:
+        input("say something to quit: -> ")
 
 
 if __name__ == "__main__":
     main()
-
-print("Done")
